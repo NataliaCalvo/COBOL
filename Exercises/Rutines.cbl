a4b356b91b7af2aef83734b08e6c3aa8ead35e84
unknown <natalia.calvo@gmail.com>
2023-05-01 22:25:15 +0200

    Whitman in routins

---
+++ Exercises/Rutines.cbl
@@ -0,0 +1,32 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. operationsRutinsWhitman.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+       RUTIN01.
+           DISPLAY "Never until the mankind making"
+           PERFORM RUTIN03.
+       RUTIN02.
+           DISPLAY "Fathering and all humbling darkness"
+           PERFORM RUTIN05.
+       RUTIN03.
+           DISPLAY "Bird beast and flower"
+           PERFORM RUTIN02.
+       RUTIN04.
+           DISPLAY "And the still hour"
+           PERFORM RUTIN06.
+       RUTIN05.
+           DISPLAY "Tells with silence the last light breaking"
+           PERFORM RUTIN04.
+       RUTIN06.
+           DISPLAY "Is come of the sea tumbling in harness."
+           STOP RUN.
+       END PROGRAM operationsRutinsWhitman.
